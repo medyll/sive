a8314f14fa72828193448eecfdb2dd8ce9b0b8af
medyll <43032494+medyll@users.noreply.github.com>
2026-03-14 17:14:01 +0100

    fix(playwright): add baseURL configuration for Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
+	use: { baseURL: 'http://localhost:4173' },
 	testDir: 'e2e'
 });
