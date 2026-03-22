88cb5de23328fa3952368d01fa9c6e64f4e22130
medyll <43032494+medyll@users.noreply.github.com>
2026-03-22 23:47:53 +0100

    feat: add @medyll/css-base dependency and import in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import './layout.css';
+	import './@medyll/css-base/app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import OfflineBanner from '$lib/elements/OfflineBanner.svelte';
 
